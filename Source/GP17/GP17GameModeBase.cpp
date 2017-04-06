480568b1e1eed1e077c2bbdf5f5ddaa31c4b6582
Marcus R. Brown <me@igetgam.es>
2017-04-05 18:05:56 -0700

    Add empty UE 4.15 Basic Code project.

---
+++ Source/GP17/GP17GameModeBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GP17.h"
+#include "GP17GameModeBase.h"
+
+
+
+
---
+++ Source/GP17/GP17GameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "GP17GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GP17_API AGP17GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
