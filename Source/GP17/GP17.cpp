444bf875fc426573c6dfe52828c16141d4e505ee
Marcus R. Brown <me@igetgam.es>
2017-05-03 19:59:00 -0700

    Delete files for project rename.

--- Source/GP17/GP17.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "GP17.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GP17, "GP17" );
--- Source/GP17/GP17GameModeBase.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "GP17.h"
-#include "GP17GameModeBase.h"
-
-
-
-
